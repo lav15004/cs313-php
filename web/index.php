57a07dc4aff6ba2461d5e7264672bf000c2899c1
lav15004 <lav15004@byui.edu>
2017-01-09 15:50:06 -0700

    added index file for group assignment and js and css for bootstrap

--- web/index.php
+++ web/index.php
@@ -1,5 +1,63 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 
-  phpinfo();
 
-?>
+  <link rel="stylesheet" href="css/week2team.css">
+  <!-- <script type="text/javascript" src="formvalidation.js"></script> -->
+
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="icon" href="../favicon.ico">
+
+  <title>02 Teach : Team Activity</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+
+
+  <!-- Custom styles for this template -->
+  <!-- <link href="navbar-fixed-top.css" rel="stylesheet"> -->
+
+
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+</head>
+
+<body>
+<div class="container">
+<div class="row">
+  <div class="col-xs-4">
+    <p id="title"><span>First Div</span></p>
+  </div>
+  <div class="col-xs-4">
+    <p id="title"><span>Second Div</span></p>
+  </div>
+  <div class="col-xs-4">
+    <p id="title"><span>Third Div</span></p>
+  </div>
+</div>
+<div class="jumbotron">
+
+    <h1>Hello, world!</h1>
+    <p>This is a template for a simple marketing or informational website.
+      It includes a large callout called a jumbotron and three supporting pieces of content.
+      Use it as a starting point to create something more unique.</p>
+
+</div>
+</div>
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="js/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="../js/jquery.min.js"><\/script>')</script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
