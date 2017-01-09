80541fb70aa74c59760b3881dd91cec8e7d2ab7f
lav15004 <lav15004@byui.edu>
2017-01-09 16:10:15 -0700

    chg small

--- web/index.php
+++ web/index.php
@@ -41,13 +41,13 @@
 
 </div>
 <div class="row">
-  <div class="col-md-4">
+  <div class="col-sm-4">
     <p id="p1"><span>First Div</span></p>
   </div>
-  <div class="col-md-4">
+  <div class="col-sm-4">
     <p id="p2"><span>Second Div</span></p>
   </div>
-  <div class="col-md-4">
+  <div class="col-sm-4">
     <p id="p3"><span>Third Div</span></p>
   </div>
 </div>
