37825916c9e8cf60b1082b62005abd50546ef093
lav15004 <lav15004@byui.edu>
2017-01-09 22:43:23 -0700

    test

--- web/assignments.php
+++ web/assignments.php
@@ -62,64 +62,8 @@
       <br />
     </div>
   </div>
-  <div id="jumbo" class="jumbotron">
-    <h2>Hello, from Aaron Lavold!</h2>
-    <p>My family and I currently live in Mesa, Arizona. I am a full-time online student that matriculated to BYU-I through the
-      Pathway program. My major is Software Engineering. I have worked for AT&T for the past 23 years. Currently, I work in AT&T's Technology Development
-      organization, as a Senior - Systems Engineer.  I love the job because it allows me to
-      work with a lot of different technologies, and it doesn't hurt that the job is fun as well. </p>
-  </div>
-  <div class="row">
-    <div class="col-sm-6">
-      <table id="schedule">
-        <tr>
-          <th colspan="5">Winter 2017 - Class Schedule</th>
-        </tr>
-        <tr>
-          <td class="scheduleth">Course #</td>
-          <td class="scheduleth">Course Description</td>
-          <td class="scheduleth">Sec#</td>
-          <td class="scheduleth">Class Location</td>
-        </tr>
-        <tr>
-          <td class="scheduletd">CS 308</td>
-          <td class="scheduletd">Technical Communication</td>
-          <td class="scheduletd">03 (Online)</td>
-          <td class="scheduletd">Online <a href="https://byui.brightspace.com/d2l/home/195575">I-Learn 3</a></td>
-        </tr>
-        <tr>
-          <td class="scheduletd">CS 313</td>
-          <td class="scheduletd">Web Engineering II</td>
-          <td class="scheduletd">02 (Online)</td>
-          <td class="scheduletd">Online <a href="https://byui.brightspace.com/d2l/home/205275">I-Learn 3</a></td>
-        </tr>
-      </table>
-    </div>
-    <div class="col-sm-1">
-    </div>
-    <div class="col-sm-6">
-      <table>
-        <tr>
-          <td>
-            <blockquote cite="https://www.lds.org/general-conference/2003/04/in-search-of-treasure?lang=eng">
-              <span id="favquote">Favorite Quote:<br /></span>“Don't save something for a special occasion. Every day in your life is a special occasion.”
-              <span id="favquotesig">-Thomas S. Monson</span>
-            </blockquote>
-          </td>
-        </tr>
-      </table>
-    </div>
-  </div>
-  <div class="row">
-    <div class="col-md-12">
-      <p id="myfamP">
-      <h2 id="myfamily">
-        My beautiful family!!!
-      </h2>
-      </p>
-      <img id="famimage" class="img-responsive img-rounded" src="../images/lavoldfamily500x316.jpg"
-           alt="Picture of the Lavold Family">
-    </div>
+  <div class="jumbotron">
+    <h1>Coming Soon !</h1>
   </div>
 </div>
 
