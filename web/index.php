7a6b5cd7e8df54d49451bb5f2b7494c270c3ab70
lav15004 <lav15004@byui.edu>
2017-01-09 20:44:15 -0700

    test

--- web/index.php
+++ web/index.php
@@ -109,10 +109,18 @@
       </table>
     </div>
     <div class="col-md-6">
-      <blockquote cite="https://www.lds.org/general-conference/2003/04/in-search-of-treasure?lang=eng" style="display:inline-block; margin-top: 5px; margin-bottom: 1em;  margin-right: 40px; height: 53px; font-family: 'monotype Corsiva'; font-size: 22pt;">
-        “Don't save something for a special occasion. Every day in your life is a special occasion.”
-        <span style ="font-weight:bold; font-style:italic;">-Thomas S. Monson</span>
-      </blockquote>
+      <table>
+        <tr>
+          <td>Favorite Quote:</td>
+        </tr>
+        <tr>
+          <blockquote cite="https://www.lds.org/general-conference/2003/04/in-search-of-treasure?lang=eng" style="display:inline-block; margin-top: 5px; margin-bottom: 1em;  margin-right: 40px; height: 53px; font-family: 'monotype Corsiva'; font-size: 22pt;">
+            “Don't save something for a special occasion. Every day in your life is a special occasion.”
+            <span style ="font-weight:bold; font-style:italic;">-Thomas S. Monson</span>
+          </blockquote>
+        </tr>
+      </table>
+
     </div>
   </div>
 
