0b7f929934342abda4fbd92a4fb0a8058c15a18a
lav15004 <lav15004@byui.edu>
2017-01-09 18:57:43 -0700

    test

--- web/js/week2team.js
+++ web/js/week2team.js
@@ -4,9 +4,8 @@ $(document).ready(function(){
         $("#div1").css("background-color",document.getElementById("colorname").value);
     });
     $("#fadetoggle").click(function(){
-        $("#div3").fadeToggle();
-        $("#div3").fadeToggle("slow");
-        $("#div3").fadeToggle(3000);
+        $("#div3").fadeToggle("slow", "linear");
+
     });
 });
 
