21a6e4a31bc7f56e63b17c8685558f9c2ee54d69
lav15004 <lav15004@byui.edu>
2017-01-09 22:25:19 -0700

    test

--- web/index.php
+++ web/index.php
@@ -41,7 +41,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#">CS 313: 02</a>
+      <a class="navbar-brand" href="#">CS 313: 02 (Online)</a>
     </div>
     <div id="navbar" class="navbar-collapse collapse">
       <ul class="nav navbar-nav">
