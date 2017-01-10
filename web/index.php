09a5532e2dcfa5c586f762642dc9e866d2d13bb6
lav15004 <lav15004@byui.edu>
2017-01-09 23:15:23 -0700

    test

--- web/index.php
+++ web/index.php
@@ -55,16 +55,16 @@
     </div><!--/.nav-collapse -->
   </div>
 </nav>
-  <div class="alert alert-success alert-dismissable">
-    <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-    <strong>Success!</strong> Indicates a successful or positive action.
-  </div>
   <div class="row">
     <div class="col-lg-12">
       <br />
       <br />
       <br />
     </div>
+  </div>
+  <div class="alert alert-success alert-dismissable">
+    <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+    <strong>Success!</strong> Indicates a successful or positive action.
   </div>
   <div id="jumbo" class="jumbotron">
     <h2>Hello, from Aaron Lavold!</h2>
