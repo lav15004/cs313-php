4fd623499526860326bfa13040e2a8b9afc94cef
lav15004 <lav15004@byui.edu>
2017-01-09 22:50:53 -0700

    test

--- web/assignments.php
+++ web/assignments.php
@@ -65,6 +65,21 @@
   <div class="jumbotron">
     <h1>Coming Soon !</h1>
   </div>
+  <div class="row">
+    <div class="col-lg-12">
+      <div class="list-group">
+        <a href="#" class="list-group-item active">
+          Cras justo odio
+        </a>
+        <a href="#" class="list-group-item">Dapibus ac facilisis in</a>
+        <a href="#" class="list-group-item">Morbi leo risus</a>
+        <a href="#" class="list-group-item">Porta ac consectetur ac</a>
+        <a href="#" class="list-group-item">Vestibulum at eros</a>
+      </div>
+    </div>
+  </div>
+  </div>
+
 </div>
 
 <!-- Bootstrap core JavaScript
