b5dbbadbaf34455b0183a085610cfa8586c16e5a
lav15004 <lav15004@byui.edu>
2017-01-19 17:30:01 -0700

    test

---
+++ web/week3formhandler.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+
+<html lang="en-us">
+
+<head>
+  <meta charset="utf-8" />
+  <title></title>
+</head>
+<body>
+<?php echo $_POST["name"]; ?><br>
+<?php echo $_POST["email"]; ?><br>
+<?php echo $_POST["major"]; ?><br>
+<?php echo $_POST["comments"]; ?><br>
+</body>
+</html>
