162b0bccbb8a3d8733b6ad256b120a354107421a
lav15004 <lav15004@byui.edu>
2017-01-19 17:47:21 -0700

    tst

--- web/week3formhandler.php
+++ web/week3formhandler.php
@@ -10,9 +10,9 @@
 <?php echo $_POST["name"]; ?><br>
 <?php echo $_POST["email"]; ?><br>
 <p>
-    <?php echo $_POST["email"]; ?>:
+
     <a href="mailto:<?php echo $_POST["email"]; ?>?Subject=Hello%20again" target="_top">
-    Send Mail</a>
+        <?php echo $_POST["email"]; ?></a>
 </p>
 <?php echo $_POST["major"]; ?><br>
 <?php echo $_POST["comments"]; ?><br>
