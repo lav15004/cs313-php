57cbe562ee4440a1d4846b41f5e35e99c19c470f
lav15004 <lav15004@byui.edu>
2017-01-20 22:29:02 -0700

    test

--- web/js/survey_validation.js
+++ web/js/survey_validation.js
@@ -1,16 +1,12 @@
 function isValid() {
-    if (document.forms["cart"]['fname'].value == ""
-        || document.forms["cart"]['lname'].value == ""
-        || document.forms["cart"]['streetaddress'].value == ""
-        || document.forms["cart"]['city'].value == ""
-        || document.forms["cart"]['state'].value == ""
-        || document.forms["cart"]['zip'].value == ""
-        || document.forms["cart"]['phonenum'].value == ""
-        || document.forms["cart"]['ccnum'].value == ""
-        || document.forms["cart"]['expiredate'].value == ""
-        || document.forms["cart"]['ccvnum'].value == ""
+    if (document.forms['bed'].value == ""
+        || document.forms['clown'].value == ""
+        || document.forms['catdog'].value == ""
+        || document.forms['sing'].value == ""
+        || document.forms['nightlight'].value == ""
+
     ) {
-        alert('Error: All Customer and Payment info form fields must be filled out.');
+        alert('Error: All survey questions must be answered.');
         return false;
     }
     return true;
