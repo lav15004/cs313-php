9f1d1bbc5e7f42e37870d164ffdd0b2e4f9826bd
lav15004 <lav15004@byui.edu>
2017-01-21 14:25:46 -0700

    test

--- web/survey_results.php
+++ web/survey_results.php
@@ -86,6 +86,11 @@
           echo "<span>$nightlight</span><br /><br />";;
         }
         // stuff to read file and display results
+        $bed_array=[];
+        $clown_array=[];
+        $catdog_array=[];
+        $sing_array=[];
+        $nightlight_array=[];
 
         $myfiler = fopen("newfile.txt", "r") or die("Unable to open file!");
         while(!feof($myfiler)) {
