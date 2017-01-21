8b23cf1dc8b950419dcd88aeb9309e48c3756f61
lav15004 <lav15004@byui.edu>
2017-01-21 16:21:19 -0700

    tsst

--- web/survey_results.php
+++ web/survey_results.php
@@ -99,7 +99,7 @@ fclose($myfiler);
       </div>
     </div>
     <div class="jumbotron">
-      <h1>Survey Results / Assignment 3 </h1>
+      <h3>Survey Results / Assignment 3 </h3>
     </div>
     <div class="row">
       <div class="col-md-12">
