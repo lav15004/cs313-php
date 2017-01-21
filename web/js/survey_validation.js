258b34a9b9d81e14f7f42d795c2cfa50cb87a3b9
lav15004 <lav15004@byui.edu>
2017-01-20 22:39:46 -0700

    test

--- web/js/survey_validation.js
+++ web/js/survey_validation.js
@@ -1,10 +1,10 @@
 function isValid() {
     alert('it ran');
     if (document.getElementById("bed").value == ""
-        || document.forms['clown'].value == ""
-        || document.forms['catdog'].value == ""
-        || document.forms['sing'].value == ""
-        || document.forms['nightlight'].value == ""
+        || document.getElementById("clown").value == ""
+        || document.getElementById("catdog").value == ""
+        || document.getElementById("sing").value == ""
+        || document.getElementById("nightlight").value == ""
     ) {
         alert('Error: All survey questions must be answered.');
         return false;
