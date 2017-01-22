e7c31cb78c9ebc9ded52093a32ee9fe1f8883dfd
lav15004 <lav15004@byui.edu>
2017-01-21 17:03:19 -0700

    test

--- web/php_survey.php
+++ web/php_survey.php
@@ -42,7 +42,7 @@
           <ul class="nav navbar-nav">
             <li><a href="index.php">Home</a></li>
             <li><a href="assignments.php">Assignments Page</a></li>
-            <li class="dropdown">
+            <li class="dropdown active">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Assignments <span class="caret"></span></a>
               <ul class="dropdown-menu">
                 <li class="active"><a href="assignments.php">Assignments Page</a></li>
