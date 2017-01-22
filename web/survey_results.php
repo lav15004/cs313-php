d9ccc4d78771d16f41bf93ea1f99205662adeeb0
lav15004 <lav15004@byui.edu>
2017-01-21 17:15:31 -0700

    test

--- web/survey_results.php
+++ web/survey_results.php
@@ -218,8 +218,6 @@ $nightlight_array = array();
   <!-- Bootstrap core JavaScript
   ================================================== -->
   <!-- Placed at the end of the document so the pages load faster -->
-  <script src="js/jquery.min.js"></script>
-  <script>window.jQuery || document.write('<script src="../js/jquery.min.js"><\/script>')</script>
-  <script src="js/bootstrap.min.js"></script>
+
 </body>
 </html>
