b507993f8dfce869799eb6bcc8dcee4de7fb580f
lav15004 <lav15004@byui.edu>
2017-01-21 17:14:08 -0700

    test

--- web/survey_results.php
+++ web/survey_results.php
@@ -36,7 +36,9 @@ $nightlight_array = array();
 
   <!-- Bootstrap core CSS -->
   <link href="css/bootstrap.min.css" rel="stylesheet">
-
+  <script src="js/jquery.min.js"></script>
+  <script>window.jQuery || document.write('<script src="../js/jquery.min.js"><\/script>')</script>
+  <script src="js/bootstrap.min.js"></script>
   <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
   <!--[if lt IE 9]>
   <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
