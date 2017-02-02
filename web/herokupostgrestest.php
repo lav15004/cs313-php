d5363662820c40902995c9716b2cb1a892d5f1e4
lav15004 <lav15004@byui.edu>
2017-02-01 17:17:32 -0700

    test

--- web/herokupostgrestest.php
+++ web/herokupostgrestest.php
@@ -9,28 +9,6 @@
 <?php
 include 'dbstuff.inc';
 
-/*print "<p>$dbUrl</p>\n\n";
-
-$dbHost = $dbopts["host"];
-echo "$dbHost<br />";
-$dbPort = $dbopts["port"];
-echo "$dbPort<br />";
-$dbUser = $dbopts["user"];
-echo "$dbUser<br />";
-$dbPassword = $dbopts["pass"];
-echo "$dbPassword<br />";
-$dbName = ltrim($dbopts["path"],'/');
-echo "$dbName<br />";
-
-print "<p>pgsql:host=$dbHost;port=$dbPort;dbname=$dbName</p>\n\n";*/
-
-try {
- $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-}
-catch (PDOException $ex) {
- print "<p>error: $ex->getMessage() </p>\n\n";
- die();
-}
 
 foreach ($db->query('SELECT now()') as $row)
 {
