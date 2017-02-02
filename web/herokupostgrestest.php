f4f320536e5bbffd58d56c980d93a92d3b7b25f6
lav15004 <lav15004@byui.edu>
2017-02-01 17:03:48 -0700

    test

--- web/herokupostgrestest.php
+++ web/herokupostgrestest.php
@@ -2,16 +2,7 @@
 <body>
 
 <?php
-
-// default Heroku Postgres configuration URL
-$dbUrl = getenv('DATABASE_URL');
-
-if (empty($dbUrl)) {
- // example localhost configuration URL with postgres username and a database called cs313db
- $dbUrl = "postgres://postgres:password@localhost:5432/cs313db";
-}
-
-$dbopts = parse_url($dbUrl);
+include 'dbstuff.inc';
 
 print "<p>$dbUrl</p>\n\n";
 
