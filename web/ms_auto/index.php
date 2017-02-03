935b9c0eae37ba4083f55bc804564405dec9bc4f
lav15004 <lav15004@byui.edu>
2017-02-02 17:02:13 -0700

    test

--- web/ms_auto/index.php
+++ web/ms_auto/index.php
@@ -26,38 +26,7 @@
 
 <body>
 <div class="container">
-<!-- Fixed navbar -->
-<nav class="navbar navbar-default navbar-fixed-top">
-  <div class="container">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="index.php">CS 313: 02 (Online)</a>
-    </div>
-    <div id="navbar" class="navbar-collapse collapse">
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="index.php">Home</a></li>
-        <li><a href="assignments.php">Assignments Page</a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Assignments <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="assignments.php">Assignments Page</a></li>
-            <li><a href="hello.html">Hello World</a></li>
-            <li><a href="week2team.php">Week 02: Team</a></li>
-            <li><a href="index.php">Week 02: Personal Homepage</a></li>
-            <li><a href="week3team.php">Week 03: Team</a></li>
-            <li><a href="php_survey.php">03 Prove : Assignment - PHP Survey</a></li>
-            <li><a href="survey_results.php">03 Prove : PHP Survey Results</a></li>
-          </ul>
-        </li>
-      </ul>
-    </div><!--/.nav-collapse -->
-  </div>
-</nav>
+  <?php include 'navbar.inc' ?>
   <div class="row">
     <div class="col-lg-12">
       <br />
