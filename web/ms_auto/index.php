939313d6cd9ad3aaba69af6ca59d7dc975bbc1be
lav15004 <lav15004@byui.edu>
2017-02-03 18:18:05 -0700

    test

--- web/ms_auto/index.php
+++ web/ms_auto/index.php
@@ -34,73 +34,9 @@
       <br />
     </div>
   </div>
-  <div id="jumbo" class="jumbotron">
-    <h2>Hello, from Aaron Lavold!</h2>
-    <p>My family and I currently live in Mesa, Arizona. I am a full-time online student that matriculated to BYU-I through the
-      Pathway program. My major is Software Engineering. I have worked for AT&T for the past 23 years. Currently, in AT&T's Technology Development
-      organization, as a Senior - Systems Engineer.  I love the job because it allows me to
-      work with a lot of different technologies, and it doesn't hurt that the job is fun too. </p>
-  </div>
-  <div class="alert alert-success alert-dismissable">
-    <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-    <strong>Attention:</strong> This page is for the "02 Prove : Assignment - Homepage" assignment.
-    All requirements have been met. Bootstrap is utilized, so you will see the page change as you resize the browser. The image at the
-    bottom will also resize if you make the browsers width narrow enough.  A few of the many "above and beyond" item are: I added this dismissable
-    alert, a jumbotron to both this page and the assignment page, a list of links to completed assignment on the assignment page,
-    and add completed assignments to the menu/nav bar.  You can dismiss this alert by clicking on the "x" in the upper right corner of this alert box.
-  </div>
-  <div class="row">
-    <div class="col-sm-6">
-      <table id="schedule">
-        <tr>
-          <th colspan="5">Winter 2017 - Class Schedule</th>
-        </tr>
-        <tr>
-          <td class="scheduleth">Course #</td>
-          <td class="scheduleth">Course Description</td>
-          <td class="scheduleth">Sec#</td>
-          <td class="scheduleth">Class Location</td>
-        </tr>
-        <tr>
-          <td class="scheduletd">CS 308</td>
-          <td class="scheduletd">Technical Communication</td>
-          <td class="scheduletd">03 (Online)</td>
-          <td class="scheduletd">Online <a href="https://byui.brightspace.com/d2l/home/195575">I-Learn 3</a></td>
-        </tr>
-        <tr>
-          <td class="scheduletd">CS 313</td>
-          <td class="scheduletd">Web Engineering II</td>
-          <td class="scheduletd">02 (Online)</td>
-          <td class="scheduletd">Online <a href="https://byui.brightspace.com/d2l/home/205275">I-Learn 3</a></td>
-        </tr>
-      </table>
-    </div>
-    <div class="col-sm-1">
-    </div>
-    <div class="col-sm-6">
-      <table>
-        <tr>
-          <td>
-            <blockquote cite="https://www.lds.org/general-conference/2003/04/in-search-of-treasure?lang=eng">
-              <span id="favquote">Favorite Quote:<br /></span>“Don't save something for a special occasion. Every day in your life is a special occasion.”
-              <span id="favquotesig">-Thomas S. Monson</span>
-            </blockquote>
-          </td>
-        </tr>
-      </table>
-    </div>
-  </div>
-  <div class="row">
-    <div class="col-md-12">
-      <p id="myfamP">
-      <h2 id="myfamily">
-        My beautiful family!!!
-      </h2>
-      </p>
-      <img id="famimage" class="img-responsive img-rounded" src="../images/lavoldfamily500x316.jpg"
-           alt="Picture of the Lavold Family">
-    </div>
-  </div>
+
+
+
 </div>
 
 <!-- Bootstrap core JavaScript
