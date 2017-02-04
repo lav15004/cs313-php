f1f3d9bfdbf2105709ffe40d5c228257ab71bb7f
lav15004 <lav15004@byui.edu>
2017-02-03 21:31:53 -0700

    test

--- web/ms_auto/search.php
+++ web/ms_auto/search.php
@@ -19,17 +19,17 @@ include 'inc/dbstuff.inc';
 
   <!-- Bootstrap core CSS -->
   <link href="../css/bootstrap.min.css" rel="stylesheet">
-
+  <script type="javascript">
+      function showOrHide(id) {
+          var elem = document.getElementById(id);
+          elem.style.visibility = (elem.style.visibility === 'hidden')? 'visible' : 'hidden';
+      }
+  </script>
   <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
   <!--[if lt IE 9]>
   <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
   <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-  <script type="javascript">
-    function showOrHide(id) {
-      var elem = document.getElementById(id);
-      elem.style.visibility = (elem.style.visibility === 'hidden')? 'visible' : 'hidden';
-    }
-  </script>
+
   <![endif]-->
 </head>
 
