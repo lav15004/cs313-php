170e5c0a3d99a4a4dae6f575cdf40a54374b2500
lav15004 <lav15004@byui.edu>
2017-02-03 23:21:06 -0700

    test

--- web/ms_auto/request.php
+++ web/ms_auto/request.php
@@ -41,10 +41,10 @@ include 'inc/dbstuff.inc';
 
   <br /><br />
   <div class="row">
-    <div class="col-lg-2">
+    <div class="col-lg-1">
       <label for="userid">User ID: </label>
     </div>
-    <div class="col-lg-10">
+    <div class="col-lg-11">
       <input type="text" name="userid" id="userid">
     </div>
   </div>
