e9ae390aac9df65b4cf63482277e3ed3af18a13a
lav15004 <lav15004@byui.edu>
2017-02-11 13:38:40 -0700

    test

--- web/ms_auto/login.php
+++ web/ms_auto/login.php
@@ -35,11 +35,11 @@
     </div>
   </div>
   <div class="row">
-    <div class="col-sm-offset-5 col-sm-1">
+    <div class="col-sm-offset-3 col-sm-2">
       <input id="loginmode" type="radio" name="mode" value="login">
       <label for="loginmode">Login</label>
     </div>
-    <div class="col-sm-1">
+    <div class="col-sm-2">
       <input id="regmode" type="radio" name="mode" value="reg">
       <label for="regmode">Register</label>
     </div>
