da87ac13ad6680e424502b7c26ff111729640c1a
lav15004 <lav15004@byui.edu>
2017-02-11 18:01:54 -0700

    test

--- web/ms_auto/inc/login-inc.php
+++ web/ms_auto/inc/login-inc.php
@@ -4,15 +4,14 @@ if ($_POST) {
     $sql_string = "select user_id, pw_hash from ms_web_users where user_id=\"".$_POST["loginuserid"]."\"";
     $statement = $db->prepare($sql_string);
 
-    $statement->execute(array(filter_var($_POST["userid"], FILTER_SANITIZE_STRING),filter_var($_POST["fullname"],
-        FILTER_SANITIZE_STRING),password_hash(filter_var($_POST["password"], FILTER_SANITIZE_STRING), PASSWORD_DEFAULT)));
+    $statement->execute();
     $newId = $db->lastInsertId('ms-web_users_id_seq');
     while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
         $user_id = $row["user_id"];
         $pw_hash = $row["pw_hash"];
     }
 
-    if (password_verify(filter_var($_POST["pw_hash"], FILTER_SANITIZE_STRING),$pw_hash)) {
+    if (password_verify(filter_var($_POST["loginpassword"], FILTER_SANITIZE_STRING),$pw_hash)) {
         echo 'valid';
     } else {
         echo 'Bad User name / password combination.';
