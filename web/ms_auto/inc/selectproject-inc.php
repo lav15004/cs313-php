66a6cde30715b9241aed1e69fe47a986b3bb32d1
lav15004 <lav15004@byui.edu>
2017-02-18 01:38:48 -0700

    test

--- web/ms_auto/inc/selectproject-inc.php
+++ web/ms_auto/inc/selectproject-inc.php
@@ -1,8 +1,7 @@
 <?php
 include 'dbstuff.inc';
-if ($_POST) {
-    $sql_string = "select ms_project_id, project_name from ms_projects where ms_server_list_id="
-        . $_GET["svalue"];
+if ($_GET) {
+    $sql_string = "select ms_project_id, project_name from ms_projects where ms_server_list_id=".$_GET['svalue'];
     $statement = $db->prepare($sql_string);
     $statement->execute();
     echo '<option value="">Select Project...</option>';
@@ -10,5 +9,7 @@ if ($_POST) {
         echo '<option value="' . $row['ms_project_id'] . '">' . $row['project_name'] . '</option>';
     }
     $db = null;
+} else {
+    echo '<option value="">Select Project...</option>';
 }
 ?>
