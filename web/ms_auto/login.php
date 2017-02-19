c5033e530ca9c74ac1ae38c02b72ad2eff3c6477
lav15004 <lav15004@byui.edu>
2017-02-19 00:05:03 -0700

    test

--- web/ms_auto/login.php
+++ web/ms_auto/login.php
@@ -126,15 +126,17 @@
       <div class="col-sm-6 text-center">
         <label class="loginFormlbl text-left" for="confpassword">Confirm Password: </label>
         <input type="text" name="confpassword" id="confpassword">
+        < /br>
+        <button class="btn btn-default btn-sm" id="btnreg">Submit</button>
       </div>
       <div class="col-sm-3">
       </div>
     </div>
     <div class="row">
-      <div class="col-sm-4">
+      <div class="col-sm-5">
       </div>
-      <div class="col-sm-4">
-        <button class="btn btn-default btn-sm text-right" id="btnreg">Submit</button>
+      <div class="col-sm-3">
+
       </div>
       <div class="col-sm-4">
       </div>
